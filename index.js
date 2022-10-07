5cdd030d707cad9637ad14a5672a697aef6fd050
Azecko <sami.panseri@epfl.ch>
2022-10-07 10:05:06 +0200

    [feat] Cleaner code, generating jdt not only for a week but for a desired timeline, ..
    - You can now specify betweeh two dates to generate the JDT. So you can make a JDT for a week, for a month, etc..
    - Code is way cleaner and "user-friendly".
    - Automatic calculation of total of hours worked and additionnal hours using [MomentJS](https://momentjs.com)

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const getSpreadSheet = require("./lib/getSpreadSheet");
 const generateReport = require("./lib/generateReport");
+const moment = require("moment");
 (async () => {
     let sheet = await getSpreadSheet();
+    var startDate = new Date("2022-09-26");
+    startDate.setDate(startDate.getDate()-1);
+    var endDate = new Date("2022-10-07");
 
-    await generateReport(sheet);
+    sheet = sheet.filter(row => {
+        if(row.morning_start && row.morning_end && row.afternoon_start && row.afternoon_end) {
+            let rowDate = moment(row.date, "DD/MM/YYYY").toDate();
+            return moment(rowDate).isBetween(startDate, endDate);
+        }
+    });
+    
+    generateReport(sheet);
     
 })();
