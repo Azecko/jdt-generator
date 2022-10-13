d4188b4978bbbbb71e2e8d8299196781f349d6fc
Azecko <sami.panseri@epfl.ch>
2022-10-13 12:16:30 +0200

    [feat] Args, command, using commander
    Only one command for the moment (to generate the MD file), exemple : `node index.js md --from=2022-01-29 --to=2022-12-12`.
    Also checking if args are valid dates.
    All of that using [Commander](https://www.npmjs.com/package/commander)

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const getSpreadSheet = require("./lib/getSpreadSheet");
 const generateReport = require("./lib/generateReport");
-const moment = require("moment");
+const { program } = require('commander');
+const moment = require("moment")
+
+let startDate;
+let endDate;
+
+function checkValidDates(fromDate, toDate) {
+    if(moment(fromDate, "YYYY-MM-DD", true).isValid() && moment(toDate, "YYYY-MM-DD", true).isValid()) {
+        return true;
+    }
+    return false;
+}
+
+program
+  .name('jdt-generator')
+  .description('Journal de travail generator coded by Azecko')
+  .version('0.8.0');
+
+program.command('md')
+  .description('Generate a journal de travail between two dates as a MarkDown file, usage : `md --from=<Date> --to=<Date>`')
+  .requiredOption('--from <date>', 'Starting date, exemple : 2022-01-30')
+  .requiredOption('--to <date>', 'End date, exemple : 2022-12-29')
+  .action((str, options) => {
+    startDate = str.from
+    endDate = str.to
+  });
+
+program.parse();
+
 (async () => {
+    if(!checkValidDates(startDate, endDate)) {
+        return console.log("Please enter two valid dates formatted like  : 2022-01-30 (YYYY-MM-DD)")
+    }
+    startDate = new Date(startDate);
+    endDate = new Date(endDate)
+
     let sheet = await getSpreadSheet();
-    var startDate = new Date("2022-09-26");
     startDate.setDate(startDate.getDate()-1);
-    var endDate = new Date("2022-10-07");
 
     sheet = sheet.filter(row => {
         if(row.morning_start && row.morning_end && row.afternoon_start && row.afternoon_end) {
